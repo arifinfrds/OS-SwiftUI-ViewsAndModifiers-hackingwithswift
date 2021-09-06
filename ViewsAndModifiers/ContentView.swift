e9f083380ae792300c8493192a400286e0d4b335
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-06 15:14:38 +0700

    make entered background of swiftui hosting preview into red

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello World")
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .background(Color.red)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
